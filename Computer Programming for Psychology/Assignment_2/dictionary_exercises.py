3c1e64e42b4c3741aa7a2fe70d761146a74182ee
sakurachaojun <sakurachaojun@gmail.com>
2022-09-26 04:14:06 -0600

    ass2_q6

---
+++ Computer Programming for Psychology/Assignment_2/dictionary_exercises.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+@author: chaojunma
+"""
+
+
+oddlist = [1,3,5,7,9]
+
+print(oddlist)
+
+len(oddlist)
+
+type(oddlist)
+
+#Create a list called "intlist"
+
+intlist = list(range(1,100+1))
+
+print(intlist)
